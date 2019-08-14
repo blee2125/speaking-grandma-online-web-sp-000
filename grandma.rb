cf2544f710a6400125982e180741eb5fece94cdf
favorable-device-5158 <blee2125@gmail.com>
2019-08-14 15:22:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,12 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    return I LOVE YOU TOO PUMPKIN!
+    return "I LOVE YOU TOO PUMPKIN!"
   end
-  if phrase != phrase.upcase
-    return HUH?! SPEAK UP, SONNY!
+  if phrase != phrase.upcase()
+    return "HUH?! SPEAK UP, SONNY!"
   else
-    return NO, NOT SINCE 1938!
+    return "NO, NOT SINCE 1938!"
   end
 
 end
